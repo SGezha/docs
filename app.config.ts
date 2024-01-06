616693c4216e7c93c202448ff849380e2ad0fc99
Sasha <sasha-gezha1@mail.ru>
2024-01-06 06:04:08 +0300

    start

---
+++ app.config.ts
@@ -0,0 +1,25 @@
+export default defineAppConfig({
+  docus: {
+    title: 'SGezha Docs',
+    description: 'Сборка фиговых кодов.',
+    socials: {
+      github: 'SGezha',
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: false
+    },
+    header: {
+      logo: false,
+      showLinkIcon: true,
+      exclude: [],
+      title: 'SGezha docs',
+      fluid: false
+    }
+  }
+})
